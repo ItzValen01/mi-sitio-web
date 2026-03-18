7348b772210ee026015cfa21a7a93f90d64725d9
ItzValen01 <gaderracrack@gmail.com>
2026-03-18 20:48:58 -0300

    Feat: agrego boton interactivo

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Sitio cargado correctamente");
+
+function saludar() {
+    console.log("Hola!");
+}
+
+function cambiarTexto() {
+    document.querySelector("h1").innerText = "Bievenido!";
+}
